05b5e853afdb785f6789b0ca973bddeb3e48d5ac
Keenen Leyson <keenen.leyson@gmail.com>
2020-06-05 00:39:21 +1200

    Created server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express') // Import express for web servers
-const server = express() // Create server
+const server = require('./server') // Create server
+
 const port = process.env.PORT || 3000 // port used for deploying
 
 // Make server listen to port 3000
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express') // Import express for web servers
+const hbs = require('express-handlebars') // Import handlebars for partials
+const routes = require('./routes')
+
+const server = express() // Create server
+
+module.exports = server // export server for routes
+
+server.engine('hbs', hbs({
+    defaultLayout: 'main',
+    extname: hbs
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({
+    extended: false
+}))
+
+server.use('/', routes)
+// server.use('/faculty', routes)
