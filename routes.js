0d6b023fce032c334ed4a3aceff2488291a0a778
Keenen Leyson <17822919+keenen1@users.noreply.github.com>
2020-06-05 13:34:28 +1200

    Fixed questions form

--- routes.js
+++ routes.js
@@ -9,30 +9,35 @@ const router = express.Router() // Create Router
 module.exports = router
 
 router.get('/', (req, res) => {
-    // Redirect root route to 'start' page
-    res.redirect('/start') 
+  // Redirect root route to 'start' page
+  res.redirect('/start')
 })
 
 router.get('/start', (req, res) => {
-    // Must haves:
-    //  1. Title: Which Teacher are you?
-    //  2. Start button redirects to First Question
-    res.render('questions/index')
-    console.log('This is the start')
+  // Must haves:
+  //  1. Title: Which Teacher are you?
+  //  2. Start button redirects to First Question
+  res.render('questions/index')
+  console.log('This is the start')
 
-    // res.redirect('/question/1')
+  // res.redirect('/question/1')
 })
 
 router.get('/question/1', (req, res) => {
-    // Dynamic route points to question pages
-    res.render('questions/question-one')
-    // res.redirect('/result')
+  // Dynamic route points to question pages
+  res.render('questions/question-one')
+  // res.redirect('/result')
 })
 
-router.get('/result', (req, res) => {
-    // Must haves:
-    //  1. Result
-    //  2. 'Start Again' button which redirects to 'start' page (Clean Slate)
-    res.send('You got Don!')
+router.post('/questions/results/:id', (req, res) => {
+    const choice = req.body
+    
+    console.log(choice)
 })
 
+// router.get('/result', (req, res) => {
+//   // Must haves:
+//   //  1. Result
+//   //  2. 'Start Again' button which redirects to 'start' page (Clean Slate)
+//   res.send('You got Don!')
+// })
